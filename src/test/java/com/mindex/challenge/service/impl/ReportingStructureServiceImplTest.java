445da70102a625b993e75d1ce526e7c4f971df19
Mihal Busho <bushomichael@gmail.com>
2020-10-05 13:19:06 -0400

    add additional test for the employee in the report

--- src/test/java/com/mindex/challenge/service/impl/ReportingStructureServiceImplTest.java
+++ src/test/java/com/mindex/challenge/service/impl/ReportingStructureServiceImplTest.java
@@ -18,7 +18,7 @@ import static org.junit.Assert.assertEquals;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ReportingStructureServiceImplTest {
     private String reportingIdUrl;
-    private String targetEmployeeID;
+
     @Autowired
     private ReportingStructureService reportingService;
 
@@ -31,12 +31,35 @@ public class ReportingStructureServiceImplTest {
     @Before
     public void setup() {
         reportingIdUrl = "http://localhost:" + port + "/reporting/{id}";
-        targetEmployeeID = "16a596ae-edd3-4847-99fe-c4518e82c86f";
     }
 
     @Test
-    public void testReporting() {
+    public void testReportingCounter() {
+        String targetEmployeeID = "16a596ae-edd3-4847-99fe-c4518e82c86f";
         ReportingStructure report = restTemplate.getForEntity(reportingIdUrl, ReportingStructure.class, targetEmployeeID).getBody();
         assertEquals(4, report.getNumberOfReports());
+    }
+
+    @Test
+    public void testReportingEmployee() {
+        String employeeId = "c0c2293d-16bd-4603-8e08-638a9d18b22c";
+        ReportingStructure report = restTemplate.getForEntity(reportingIdUrl, ReportingStructure.class, employeeId).getBody();
+
+        //Create expected Employee
+        Employee expectedEmployee = new Employee();
+        expectedEmployee.setFirstName("George");
+        expectedEmployee.setLastName("Harrison");
+        expectedEmployee.setDepartment("Engineering");
+        expectedEmployee.setPosition("Developer III");
+
+        assertEmployeeEquivalence(expectedEmployee, report.getEmployee());
+    }
+
+    private static void assertEmployeeEquivalence(Employee expected, Employee actual) {
+        assertEquals(expected.getFirstName(), actual.getFirstName());
+        assertEquals(expected.getLastName(), actual.getLastName());
+        assertEquals(expected.getDepartment(), actual.getDepartment());
+        assertEquals(expected.getPosition(), actual.getPosition());
+        assertEquals(expected.getDirectReports(), actual.getDirectReports());
     }
 }
